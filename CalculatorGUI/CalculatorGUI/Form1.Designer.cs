94a3c508a9cb56eed8f4201eabf7736128eef236
Filip "Filipunk" Lafata <84471676+Filipunk15@users.noreply.github.com>
2024-11-19 06:51:38 +0100

    Add files via upload

---
+++ CalculatorGUI/CalculatorGUI/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿using System.Drawing;
+
+namespace CalculatorGUI
+{
+    partial class Calcu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calcu));
+            this.button9 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.buttonCE = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonDivide = new System.Windows.Forms.Button();
+            this.buttonResult = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_click);
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_click);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            // 
+            // button10
+            // 
+            resources.ApplyResources(this.button10, "button10");
+            this.button10.Name = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button_click);
+            // 
+            // buttonCE
+            // 
+            resources.ApplyResources(this.buttonCE, "buttonCE");
+            this.buttonCE.Name = "buttonCE";
+            this.buttonCE.UseVisualStyleBackColor = true;
+            this.buttonCE.Click += new System.EventHandler(this.buttonCE_Click);
+            // 
+            // buttonPlus
+            // 
+            resources.ApplyResources(this.buttonPlus, "buttonPlus");
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonMinus
+            // 
+            resources.ApplyResources(this.buttonMinus, "buttonMinus");
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonMultiply
+            // 
+            resources.ApplyResources(this.buttonMultiply, "buttonMultiply");
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // buttonDivide
+            // 
+            resources.ApplyResources(this.buttonDivide, "buttonDivide");
+            this.buttonDivide.Name = "buttonDivide";
+            this.buttonDivide.UseVisualStyleBackColor = true;
+            this.buttonDivide.Click += new System.EventHandler(this.buttonDivide_Click);
+            // 
+            // buttonResult
+            // 
+            resources.ApplyResources(this.buttonResult, "buttonResult");
+            this.buttonResult.Name = "buttonResult";
+            this.buttonResult.UseVisualStyleBackColor = true;
+            this.buttonResult.Click += new System.EventHandler(this.buttonResult_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            // 
+            // Calcu
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            resources.ApplyResources(this, "$this");
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonResult);
+            this.Controls.Add(this.buttonDivide);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonCE);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button9);
+            this.MaximizeBox = false;
+            this.Name = "Calcu";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button buttonCE;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonDivide;
+        private System.Windows.Forms.Button buttonResult;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ CalculatorGUI/CalculatorGUI/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculatorGUI
+{
+    public partial class Calcu : Form
+    {
+        private string value;
+        private int signNum;
+        private int firstValue;
+        private int secondValue;
+        public Calcu()
+        {
+            InitializeComponent();
+            textBox1.Text = "0";
+            signNum = 0;
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Button btn = (System.Windows.Forms.Button)sender;
+            value = btn.Text;
+            if (value.Length < textBox1.MaxLength)
+            {
+                changeValue();
+            }
+
+        }
+
+        private void changeValue()
+        {
+            value = value.ToString();
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = value;
+            }
+            else
+            {
+                textBox1.Text += value;
+            }
+        }
+
+        private void buttonCE_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            getResults();
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            signNum = 1;
+            textBox2.Text = textBox1.Text;
+            textBox1.Text = "0";
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            signNum = 2;
+            textBox2.Text = textBox1.Text;
+            textBox1.Text = "0";
+
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            signNum = 3;
+            textBox2.Text = textBox1.Text;
+            textBox1.Text = "0";
+        }
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            signNum = 4;
+            textBox2.Text = textBox1.Text;
+            textBox1.Text = "0";
+        }
+
+        private int getResults()
+        {
+            switch (signNum)
+            {
+                case 1:
+                    {
+                        firstValue = Convert.ToInt32(textBox2.Text);
+                        secondValue = Convert.ToInt32(textBox1.Text);
+                        textBox2.Text = firstValue.ToString() + " + " + secondValue.ToString();
+                        int result = firstValue + secondValue;
+                        textBox1.Text = result.ToString();
+                        return firstValue + secondValue;
+                    }
+                case 2:
+                    {
+                        firstValue = Convert.ToInt32(textBox2.Text);
+                        secondValue = Convert.ToInt32(textBox1.Text);
+                        textBox2.Text = firstValue.ToString() + " - " + secondValue.ToString();
+                        int result = firstValue - secondValue;
+                        textBox1.Text = result.ToString();
+                        return firstValue - secondValue;
+                    }
+                case 3:
+                    {
+                        firstValue = Convert.ToInt32(textBox2.Text);
+                        secondValue = Convert.ToInt32(textBox1.Text);
+                        textBox2.Text = firstValue.ToString() + " * " + secondValue.ToString();
+                        int result = firstValue * secondValue;
+                        textBox1.Text = result.ToString();
+                        return firstValue * secondValue;
+                    }
+                case 4:
+                    {
+                        firstValue = Convert.ToInt32(textBox2.Text);
+                        secondValue = Convert.ToInt32(textBox1.Text);
+                        if (secondValue == 0)
+                        {
+                            textBox1.Text = "Neplatne zadani!";
+                            return 0;
+                        }
+                        else
+                        {
+                            textBox2.Text = firstValue.ToString() + " / " + secondValue.ToString();
+                            int result = firstValue / secondValue;
+                            textBox1.Text = result.ToString();
+                            return firstValue / secondValue;
+                        }
+                    }
+                default:
+                    return 0;
+            }
+        }
+    }
+}
